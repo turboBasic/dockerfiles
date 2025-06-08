dc15a554bf76b22b3c56df20be9b8cc96c0ebd5e
turboBasic ⚡️💀 <andrii7melnyk@proton.me>
2025-06-08 14:05:32 +0200

    feat(ubuntu24): prepare build for different versions of Ubuntu

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DIR_NAME="$1"
-LOCALE="${2:-en_DK.UTF-8}"
-TIMEZONE="${3:-Europe/Berlin}"
+TAG="${2:-$1}"
+LOCALE="${3:-en_DK.UTF-8}"
+TIMEZONE="${4:-Europe/Berlin}"
 
 if [ -z "$DIR_NAME" ]; then
     echo "Usage: $0 <directory_name>"
@@ -13,10 +14,15 @@ if [ ! -d "$DIR_NAME" ]; then
     exit 1
 fi
 
-docker buildx build --tag "$DIR_NAME" --file "$DIR_NAME"/Dockerfile \
-    --build-arg platform=linux/amd64 \
+image_tag="${TAG%:*}"
+image_version="${TAG#*:}"  # Replace slashes with dashes for Docker tag compatibility
+docker buildx build \
+    --platform=linux/amd64 \
+    --tag "$image_tag" \
+    --file "$DIR_NAME"/Dockerfile \
+    --build-arg version="$image_version" \
     --build-arg locale="$LOCALE" \
     --build-arg timezone="$TIMEZONE" \
     --build-arg userName=mao \
-    --build-arg name=$(basename "${DIR_NAME%/}") \
+    --build-arg homeDir="$(basename "${DIR_NAME%/}")/home" \
     .
